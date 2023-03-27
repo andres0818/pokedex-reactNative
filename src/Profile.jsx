f2af3a87ddd311acb47a70bb91b39b408346e7b0
andres0818 <c.andres.perez.h@gmail.com>
2023-03-27 00:09:09 -0500

    change title page

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,13 +1,17 @@
 import { StyleSheet, Text } from "react-native";
+import { PokedexTitle } from "./IconNavBar/IconNavBar";
 
-const ProfileScreen = ({route}) => {
-    return <Text style={styles.text}>This is {route.params.name}'s profile and have {route.params.age} old's</Text>;
-  };
+const ProfileScreen = ({ route, navigation }) => {
+  navigation.setOptions({
+    headerTitle: () => <PokedexTitle icon={route.params.icon} title={route.params.title} />,
+  });
+  return <Text style={styles.text}>This is </Text>;
+};
 
-export default ProfileScreen
+export default ProfileScreen;
 
 const styles = StyleSheet.create({
-text:{
-    color:"black"
-}
-})
+  text: {
+    color: "black",
+  },
+});
