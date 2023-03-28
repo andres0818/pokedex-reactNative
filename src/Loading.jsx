a70279f032dcefdae6bf7491409830ee086de74d
andres0818 <c.andres.perez.h@gmail.com>
2023-03-27 21:08:07 -0500

    create component

---
+++ src/Loading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ActivityIndicator } from "react-native/";
+
+const Loading = () => {
+  return <ActivityIndicator size="large" color="#00b7ff" />;
+};
+
+export default Loading;
